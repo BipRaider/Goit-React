657d4662c556d748cb6eb0441961dfe8246430fa
BipRaider <thebipus@gmail.com>
2020-08-30 17:44:49 +0300

    Updates

---
+++ precache-manifest.f5470ea391dcad6fb5636cd2cbeb6a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea02e37afa5b030eab702f1cf96505d9",
+    "url": "/Goit-React/index.html"
+  },
+  {
+    "revision": "4930673b4f325a186c71",
+    "url": "/Goit-React/static/css/main.d13d49cc.chunk.css"
+  },
+  {
+    "revision": "675ae7f9986abcb1604c",
+    "url": "/Goit-React/static/js/2.9912dd50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Goit-React/static/js/2.9912dd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4930673b4f325a186c71",
+    "url": "/Goit-React/static/js/main.2d426637.chunk.js"
+  },
+  {
+    "revision": "a720164b6bec2199f4f5",
+    "url": "/Goit-React/static/js/runtime-main.f2f8d8cd.js"
+  }
+]);
